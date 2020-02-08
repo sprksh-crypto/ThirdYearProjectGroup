51b18762123f612e913483f408620a21a57fea35
Zhi Jun Cheung <50379115+jmanchuck@users.noreply.github.com>
2020-02-08 20:50:28 +0000

    Add files via upload

---
+++ Scripts/CamManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamManager : MonoBehaviour
+{
+    public static Camera[] cameras;
+    private int currentCameraIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameras = FindObjectsOfType<Camera>();
+        for (int i = 1; i < cameras.Length; i++)
+        {
+            cameras[i].gameObject.SetActive(false);
+        }
+
+        if (cameras.Length > 0)
+        {
+            cameras[0].gameObject.SetActive(true);
+        }
+    }
+
+    private void InputChangeCam()
+    {
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            cameras[currentCameraIndex].gameObject.SetActive(false);
+            currentCameraIndex += 1;
+            if (currentCameraIndex == cameras.Length)
+            {
+                currentCameraIndex = 0;
+            }
+            cameras[currentCameraIndex].gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        InputChangeCam();
+    }
+}
---
+++ Scripts/GravityBody.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityBody : MonoBehaviour
+{
+
+    // Define attributes for convenience
+    private Transform bodyTransform;
+    private float mass;
+    private Rigidbody rb;
+
+    // Allow user to tweak
+    public Vector3 initVelocity; // Allow input via inspector
+    public GravityBody OrbitTarget = null; // The body that THIS body will orbit around (drag and drop at inspector)
+    public Vector3 OrbitNormal;
+
+    // G - arbitrary
+    public const float G = 100000f;
+
+    // List of all gravitational bodies
+    private static List<GravityBody> AllBodies = new List<GravityBody>();
+
+    private void Awake()
+    {
+        // Get components of the body
+        rb = GetComponent<Rigidbody>();
+        bodyTransform = GetComponent<Transform>();
+
+        // Set attributes
+        mass = rb.mass;
+
+        AllBodies.Add(this);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GravityBody body in AllBodies)
+        {
+            // Set orbital velocity, if a body to orbit around is specified 
+            if (body != OrbitTarget && body.initVelocity == Vector3.zero && OrbitTarget != null)
+            {
+                // If user does not specify a normal vector, set as Y vector (XZ plane)
+                if (OrbitNormal == Vector3.zero)
+                {
+                    OrbitNormal = new Vector3(0, 1, 0);
+                }
+                SetOrbitalVelocity(OrbitNormal);
+            }
+        }
+        SetVelocity(initVelocity);
+    }
+
+    // Attract another gravitational body 
+    public void Attract(GravityBody gb)
+    {
+        // Vector from target body to this body
+        Vector3 r_to_this = this.bodyTransform.position - gb.bodyTransform.position;
+
+        // Newton's law of gravitation
+        Vector3 force = r_to_this * G * gb.GetMass() * this.GetMass() / Mathf.Pow(r_to_this.magnitude, 3);
+        gb.SetForceTo(force);
+    }
+
+    // Set this body to orbit around some other CentreBody, tangential to some normal vector
+    private void SetOrbitalVelocity(Vector3 normal)
+    {
+        // Vector pointing towards the orbit target
+        Vector3 r_to_target = this.bodyTransform.position - OrbitTarget.bodyTransform.position;
+
+        // Magnitude 
+        float magnitude = r_to_target.magnitude;
+        Vector3 tangentDirection = Vector3.Cross(normal, r_to_target);
+        Vector3 orbitalVelocity = Mathf.Sqrt(G * OrbitTarget.GetMass() / Mathf.Pow(magnitude, 3)) * tangentDirection;
+
+        // Setting this body's initial velocity as the calculated orbit velocity
+        initVelocity = orbitalVelocity;
+        Debug.Log(initVelocity);
+    }
+
+    // Update is called once per x frames
+    void FixedUpdate()
+    {
+        // N^2 interactions, each body acts on all others
+        foreach (GravityBody body in AllBodies)
+        {
+            if (body != this)
+            {
+                this.Attract(body);
+            }
+        }
+    }
+
+    //// Get Set methods ////
+    public float GetMass()
+    {
+        return mass;
+    }
+
+    public void SetVelocity(Vector3 velocity)
+    {
+        rb.velocity = velocity;
+    }
+
+    public void SetForceTo(Vector3 force)
+    {
+        rb.AddForce(force);
+    }
+}
