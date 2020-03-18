1ee21b049db6d0a7cb59e4899568f0fe926ed42a
1geYang <60508372+1geYang@users.noreply.github.com>
2020-03-18 21:40:32 +0000

    Add files via upload

---
+++ Yige - Gravity Plus Moon/Attractor copy.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Attractor : MonoBehaviour
+{
+    private Rigidbody rb;
+
+
+    public const float G = 0.025f;
+
+    public Vector3 OrbitNormal;
+    public Vector3 Initvelocity;
+    public float semiMajorAxis;
+    public Attractor target = null;
+    public static List<Attractor> attractors = new List<Attractor>();
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        attractors.Add(this);
+    }
+
+    private void Start()
+    {
+
+        if (target != null && Initvelocity == Vector3.zero)
+        {
+            SetOrbitalVelocity(OrbitNormal);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (Attractor attractor in attractors)
+        {
+            if (attractor != this) Attract(attractor);
+        }
+    }
+
+    internal int GetRadius()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Attract(Attractor obToAttract)
+    {
+
+        Rigidbody rbToAtrract = obToAttract.rb;
+        Vector3 direction = rb.position - rbToAtrract.position;
+        float distance = direction.magnitude;
+
+
+        float forceMagnitude = (G * rb.mass * rbToAtrract.mass) / Mathf.Pow(distance, 2);
+        Vector3 force = direction.normalized * forceMagnitude;
+
+        rbToAtrract.AddForce(force);
+    }
+
+    void SetOrbitalVelocity(Vector3 normal)
+
+        
+
+
+    {
+    
+        if (normal == Vector3.zero)
+        {
+            normal = new Vector3(0, 1, 0);
+        }
+
+
+        Vector3 direction = rb.position - target.rb.position;
+        float distance = direction.magnitude;
+
+        Vector3 unitTangent = Vector3.Cross(normal, direction.normalized);
+     
+
+        if (semiMajorAxis == 0)
+        {
+            semiMajorAxis = distance;
+        }
+
+        
+        Vector3 InitVelocity = Mathf.Sqrt((G * target.rb.mass) * ((2 / distance) - (1 / (semiMajorAxis)))) * unitTangent;
+
+
+        this.rb.velocity = InitVelocity;
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
---
+++ Yige - Gravity Plus Moon/Moonvelocity copy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class Moonvelocity : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float semiMajorAxis;
+    public const float G = 0.025f;
+    private const float Earthmass = 0.015f;
+    public Vector3 OrbitNormal;
+    public Vector3 Initvelocity;
+
+    private const float Sunmass = 33000f;
+
+
+    void SetOrbitalVelocity(Vector3 normal)
+
+
+
+    {
+        if (normal == Vector3.zero)
+        {
+            normal = new Vector3(0, 1, 0);
+        }
+
+
+        Vector3 moondistance = rb.position - GameObject.Find("Earth").transform.position;
+        float moon2earth = moondistance.magnitude;
+        Vector3 moonunitTangent = Vector3.Cross(normal, moondistance.normalized);
+
+
+
+        Vector3 rbdistance = GameObject.Find("Earth").transform.position - GameObject.Find("Sun").transform.position;
+
+        Vector3 unitTangent = Vector3.Cross(normal, rbdistance.normalized);
+       float e2sdistance = rbdistance.magnitude; //moon to sun distance
+
+        Vector3 EarthInitVelocity = Mathf.Sqrt(G * Sunmass / e2sdistance) * unitTangent;
+
+        Vector3 OrbitalVelocityMoon2Earth = Mathf.Sqrt(G * Earthmass /moon2earth) * moonunitTangent;
+
+
+
+
+        //if (semiMajorAxis == 0)
+
+          //{semiMajorAxis = e2sdistance;}
+
+
+
+        //Vector3 EarthInitVelocity = Mathf.Sqrt((G * Sunmass) * ((2 / e2sdistance) - (1 / (semiMajorAxis)))) * unitTangent;
+        //Vector3 OrbitalVelocityMoon2Earth = Mathf.Sqrt((G * Earthmass) * ((2 / moon2earth) - (1 / (semiMajorAxis)))) * moonunitTangent;
+
+
+
+
+        Vector3 MoonVelocity = EarthInitVelocity + OrbitalVelocityMoon2Earth;
+
+        this.rb.velocity = MoonVelocity;
+
+
+
+    }
+}
+
+
